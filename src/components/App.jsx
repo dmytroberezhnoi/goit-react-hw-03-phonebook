c9318eb76e495bf6b7909c0b0a08d74e8984964d
Dmytro Berezhnoi <berezhnoi.dmytro@gmail.com>
2023-08-10 10:29:49 +0200

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,13 +7,6 @@ import { СontactList } from './ContactList/ContactList';
 
 import css from './App.module.css';
 
-// const startContacts = [
-//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-// ];
-
 export class App extends React.Component {
   state = {
     contacts: [
@@ -26,7 +19,6 @@ export class App extends React.Component {
   };
 
   componentDidMount() {
-    // localStorage.setItem(`contacts`, JSON.stringify(startContacts));
     const data = JSON.parse(localStorage.getItem(`contacts`));
     if (data) {
       this.setState({
